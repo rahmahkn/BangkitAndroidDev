3a5777a62cb34e732efcb22110b5d7b3b546ae94
rahmahkn <rahmah.ks.nurdini@gmail.com>
2022-05-07 15:25:08 +0700

    Fix dir

---
+++ 5. Belajar Pengembangan Aplikasi Android Intermediate/Submission 2/app/src/main/java/com/example/storyapp/helper/MainViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.storyapp.helper
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import com.example.storyapp.network.ApiService
+
+class MainViewModel(private val api: ApiService, private val token: String) : ViewModel() {
+    val quote = Pager(
+        config = PagingConfig(pageSize = 5),
+        pagingSourceFactory = {
+            StoryPagingDataSource(api, token)
+        }).flow.cachedIn(viewModelScope)
+}
+
